b467ae1c243e76cb2aae36934662bddfa96f704d
Felix342 <fillebraberg@gmail.com>
2020-11-27 17:17:21 +0100

    Refactor: added a folder structure for submodules

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -26,55 +26,5 @@ module Utils where
 
 import Data.List
 
------------------------------------Math-----------------------------------
--- |Calculates the median value of a list
-median :: (Fractional a, Ord a) => [a] -> a
-median lst | odd (length lst) = midItem1
-           | otherwise        = midItem2
-   where
-       sortedLst = sort lst
-       midItem1  = head $ take 1 $ drop (div (length sortedLst) 2) sortedLst
-       midItem2  = mean $ take 2 $ drop (div (length sortedLst) 2-1) sortedLst
-
--- |Calculates the mean value of a list
-mean :: Fractional a => [a] -> a
-mean lst = sum lst / fromIntegral (length lst)
-
--- |Calculates the fibonacci sequence given a length
-fibs :: Int -> [Int]
-fibs n = map fib [1..n]
-
--- |Calculates the n:th fibonacci number
-fib :: Int -> Int
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n - 1) + fib (n - 2)
-
--- |Indentity matrix of given size
-idMatrix = undefined
-
-
------------------------------------Conversion-----------------------------------
--- | Converts 10-base Integer to binary.
-toBinary :: Integer -> Integer
-toBinary = undefined
-
--- | Converts from binary to 10-base Integer.
-fromBinary :: Integer -> Integer
-fromBinary = undefined
-
--- | Converts 10-base Integer to hexadecimal.
-toHex :: Integer -> Integer
-toHex = undefined
-
--- | Converts from hexadecimal to 10-base Integer.
-fromHex :: Integer -> Integer
-fromHex = undefined
-
--- | Converts 10-base Integer  to octadecimal.
-toOcta :: Integer -> Integer
-toOcta = undefined
-
--- | Converts octadecimal to 10-base Integer.
-fromOcta :: Integer -> Integer
-fromOcta = undefined
+import Maths.Maths
+import Conversion.Conversion
