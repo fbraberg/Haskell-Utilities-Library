88f0485ece79bf63719b5be796df7c2b1dd905e6
Robin S <Cogsad@protonmail.com>
2020-11-27 17:13:32 +0100

    Add: added documentation

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -53,20 +53,26 @@ idMatrix = undefined
 --vectorMult
 
 -----------------------------------Conversion-----------------------------------
+-- | Converts 10-base Integer to binary.
 toBinary :: Integer -> Integer
 toBinary = undefined
 
+-- | Converts from binary to 10-base Integer.
 fromBinary :: Integer -> Integer
 fromBinary = undefined
 
+-- | Converts 10-base Integer to hexadecimal.
 toHex :: Integer -> Integer
 toHex = undefined
 
+-- | Converts from hexadecimal to 10-base Integer.
 fromHex :: Integer -> Integer
 fromHex = undefined
 
+-- | Converts 10-base Integer  to octadecimal.
 toOcta :: Integer -> Integer
 toOcta = undefined
 
+-- | Converts octadecimal to 10-base Integer.
 fromOcta :: Integer -> Integer
 fromOcta = undefined
