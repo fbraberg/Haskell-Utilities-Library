962e3c34b87224e51dfeac4709f5764a4c0e558d
Felix342 <fillebraberg@gmail.com>
2020-11-28 10:54:06 +0100

    Working on test funcs for Maths.hs

--- lib/Maths/Maths.hs
+++ lib/Maths/Maths.hs
@@ -4,6 +4,7 @@ module Maths.Maths where
 
 import Data.List
 import Data.List.Split
+import Test.QuickCheck
 
 -- |Calculates the median value of a list
 median :: (Fractional a, Ord a) => [a] -> a
@@ -13,6 +14,13 @@ median lst | odd (length lst) = midItem1
        sortedLst = sort lst
        midItem1  = head $ take 1 $ drop (div (length sortedLst) 2) sortedLst
        midItem2  = mean $ take 2 $ drop (div (length sortedLst) 2-1) sortedLst
+
+prop_MedianLength :: (Fractional a, Ord a) => [a] -> Bool
+prop_MedianLength list = (m `elem` list) &&
+                         (take 1 (drop (halfLen-1) list)) == [m]   -- This needs testing!
+    where
+        m = median list
+        halfLen = (length list) `div` 2
 
 -- |Calculates the mean value of a list
 mean :: Fractional a => [a] -> a
