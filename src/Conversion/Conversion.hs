cce5e737cf2e93b664d859907b0706b72a952e35
Robin S <Cogsad@protonmail.com>
2020-11-29 18:10:05 +0100

    Add: added property function

--- src/Conversion/Conversion.hs
+++ src/Conversion/Conversion.hs
@@ -1,27 +1,33 @@
 module Conversion.Conversion where
+import Test.QuickCheck
 
 -- | Converts 10-base Integer to binary.
-toBinary :: Int -> Int
+toBinary :: Integer -> Integer
 toBinary n = read (concat(map show(toBinaryList n)))
   
 
-toBinaryList :: Int -> [Int]
+toBinaryList :: Integer -> [Integer]
 toBinaryList 0 = [0]
 toBinaryList n | n `mod` 2 == 1 = (toBinaryList (n `div` 2) ++ [1])
                | n `mod` 2 == 0 = (toBinaryList (n `div` 2) ++ [0])
            
 
 -- | Converts from binary to 10-base Integer.
-fromBinary :: Int -> Int
+fromBinary :: Integer -> Integer
 fromBinary n = fromBinaryList(ns)
-  where ns = map (\x -> read [x] :: Int) (show n)
+  where ns = map (\x -> read [x] :: Integer) (show n)
 
-fromBinaryList :: [Int] -> Int
+fromBinaryList :: [Integer] -> Integer
 fromBinaryList xs = sum (zipWith (*) calcList (reverse xs))
   where
     twoList = map (\x -> if x > 0 then x+1 else x) xs -- | Converts 1s to 2s in the list.
     calcList = zipWith (^) (reverse twoList) [0,1..] -- | Calculates the value for each element.
 
+
+prop_binary :: Gen Bool
+prop_binary = do
+  x <- choose (1,10000) :: Gen Integer
+  return $ x == fromBinary(toBinary x)
 
 -- | Converts 10-base Integer to hexadecimal.
 toHex :: Integer -> Integer
