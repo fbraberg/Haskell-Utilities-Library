27b04ec5d57ac0d14338ee8f8fc96108dcb374cc
Felix Braberg <fillebraberg@gmail.com>
2020-11-30 18:27:09 +0100

    Add: work in progress createBST

--- src/Datastructures/BinarySearchTree.hs
+++ src/Datastructures/BinarySearchTree.hs
@@ -11,23 +11,30 @@ data BST a = BST (Node a) deriving (Show)
 -- ==========================/Node\==========================
 data Node a = Node {keyVal :: Int
                    ,val :: a
-                   ,left :: Maybe (Node a)
-                   ,right :: Maybe (Node a)} deriving (Eq)
+                   ,left :: Node a
+                   ,right :: Node a} | End deriving (Eq)
 
 instance Show a => Show (Node a) where
-    show (Node key val _ _) =
-        ("Key: " ++ (show key) ++ " Value: " ++ show val ++ "\n")
+    show (Node key val left right) =
+        ("Key: " ++ (show key) ++ " Value: " ++ show val ++
+         "Left node: " ++ (show left) ++ "Right node: " ++ (show right) ++ "\n") -- Fix non exhaustive pattern to see if createBST works
 
 instance Eq a => Ord (Node a) where
     (<=) (Node key1 val1 _ _) (Node key2 val2 _ _) = key1 <= key2
     (>=) (Node key1 val1 _ _) (Node key2 val2 _ _) = key1 >= key2
 -- ==========================\Node/==========================
 
-nodes = map (\(k,v)->createNode k v Nothing Nothing) $ zip  [1..10] ['a'..'j']
+nodes = map (\(k,v)->createNode k v End End) $ zip  [1..10] ['a'..'j']
 
-createBST :: Ord a => [Node a] -> BST a
-createBST nodes = undefined
+createBST :: Ord a => [Node a] -> Node a
+createBST [Node k v End End] = Node k v End End
+createBST nodes = Node k v leftBranch rightBranch
+    where
+        midIndex = div (length nodes) 2
+        Node k v _ _ = head $ drop midIndex nodes
+        leftBranch = createBST $ take midIndex nodes
+        rightBranch = createBST $ drop midIndex nodes
 
 
-createNode :: Int -> a -> Maybe (Node a) -> Maybe (Node a)  -> Node a
+createNode :: Int -> a -> Node a -> Node a  -> Node a
 createNode key val left right = Node key val left right
