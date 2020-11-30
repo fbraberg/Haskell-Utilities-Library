da9cd2113b1a498f24dfc9d3a5e0fec63ac3de07
Felix Braberg <fillebraberg@gmail.com>
2020-11-30 19:45:47 +0100

    Fix: made a createBST helper func to have correct type signatures

--- src/Datastructures/BinarySearchTree.hs
+++ src/Datastructures/BinarySearchTree.hs
@@ -26,17 +26,23 @@ instance Eq a => Ord (Node a) where
     (>=) (Node key1 val1 _ _) (Node key2 val2 _ _) = key1 >= key2
 -- ==========================\Node/==========================
 
-mNodes = map (\(k,v)->createNode k v End End) $ zip  [1..10] ['a'..'j']
-sNodes = map (\(k,v)->createNode k v End End) $ zip  [1..3] ['a'..'c']
+lNodes = map (\(k,v)->createNode k v End End) $ zip  [1..]
+                        [[c1] ++ [c2] | c1 <- ['a'..'z'], c2 <- ['a'..'z']]
+mNodes = map (\(k,v)->createNode k v End End) $ zip  [1..] ['a'..'j']
+sNodes = map (\(k,v)->createNode k v End End) $ zip  [1..] ['a'..'c']
 
-createBST :: Ord a => [Node a] -> Node a
-createBST [Node k v End End] = Node k v End End
-createBST nodes = Node k v leftBranch rightBranch
+createBST :: Ord a => [Node a] -> BST a
+createBST nodes = BST $ createBST_helper nodes
+
+createBST_helper :: Ord a => [Node a] -> Node a
+createBST_helper [Node k v End End] = Node k v End End
+createBST_helper nodes = Node k v leftBranch rightBranch
     where
         midIndex = div (length nodes) 2
         Node k v _ _ = head $ drop midIndex nodes
-        leftBranch = createBST $ take midIndex nodes
-        rightBranch = createBST $ drop (midIndex+1) nodes
+        leftBranch = createBST_helper $ take midIndex nodes
+        rightBranch = createBST_helper $ drop (midIndex+1) nodes
+
 
 
 createNode :: Int -> a -> Node a -> Node a  -> Node a
