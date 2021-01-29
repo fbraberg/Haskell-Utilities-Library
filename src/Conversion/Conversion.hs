63cd66c58e1a81c9ba0249af040675947623b718
Robin S <Cogsad@protonmail.com>
2021-01-29 20:30:43 +0100

    Add: added datatypes for Binary and Bit

--- src/Conversion/Conversion.hs
+++ src/Conversion/Conversion.hs
@@ -1,31 +1,37 @@
-module Conversion(toBinary, fromBinary) where
+module Conversion(toBinary, fromBinary, Bit, Bin) where
+
+data Bit = I | O deriving (Eq, Show)
+data Bin = Bin { bits :: [Bit] } deriving Eq
 
 
+instance Show Bin where
+  show = concatMap (show . b2i) . bits
+
+test = Bin [I,O,I,O,I,O]
+
+-- | Function to covert Bit into Integer
+b2i :: Bit -> Integer
+b2i I = 1
+b2i O = 0
 
 -- | Converts 10-base Integer to binary.
-toBinary :: Integer -> String
-toBinary n = concat(map show(toBinaryList n))
+toBinary :: Integer -> Bin
+toBinary n = Bin(toBinaryList n)
 
 
-toBinaryList :: Integer -> [Integer]
-toBinaryList 0 = [0]
-toBinaryList n | n `mod` 2 == 1 = (toBinaryList (n `div` 2) ++ [1])
-               | n `mod` 2 == 0 = (toBinaryList (n `div` 2) ++ [0])
+
+toBinaryList :: Integer -> [Bit]
+toBinaryList 0 = []
+toBinaryList n | n `mod` 2 == 1 = (toBinaryList (n `div` 2) ++ [I])
+               | n `mod` 2 == 0 = (toBinaryList (n `div` 2) ++ [O])
 
 
 -- | Converts from binary to 10-base Integer.
-fromBinary :: String -> Integer
-fromBinary n = fromBinaryList(ns)
-  where ns = map (\x -> read [x] :: Integer) n
-
-fromBinaryList :: [Integer] -> Integer
-fromBinaryList xs = sum (zipWith (*) calcList (reverse xs))
+fromBinary :: Bin -> Integer
+fromBinary (Bin xs) = sum (zipWith (*) calcList (reverse (map (b2i) xs)))
   where
-    twoList = map (\x -> if x > 0 then x+1 else x) xs -- | Converts 1s to 2s in the list.
-    calcList = zipWith (^) (reverse twoList) [0,1..] -- | Calculates the value for each element.
-
-
-
+    twoList = map (\x -> if x == I then (b2i x)+1  else b2i x) xs
+    calcList = zipWith(^) (reverse twoList) [0,1..]
 
 
 -- | Converts 10-base Integer to hexadecimal.
